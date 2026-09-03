4caa484f690b34b1e9bd1d4a5b28039b5cda42dc
Aditya Vikram <adityavikram803@gmail.com>
2026-09-03 08:00:56 +0530

    Refactor Credential class to be abstract
    Refactored Credential class to be abstract and removed site and category fields.

--- app/src/main/java/com/yapm/android/Credential.java
+++ app/src/main/java/com/yapm/android/Credential.java
@@ -1,66 +1,31 @@
 package com.yapm.android;
 
-public class Credential {
-private String site;
-private String username;
-private String password;
-private String notes;
-private String category;
+public abstract class Credential {
+    private String username;
+    private String password;
+    private String notes;
 
-public Credential() {}
-public Credential(String site, String username, String password, String notes, String category) {
-    this.site = site;
-    this.username = username;
-    this.password = password;
-    this.notes = notes;
-    this.category = category;
-}
+    public Credential() {}
 
-public Credential(Credential other) {
-    this.site = other.site;
-    this.username = other.username;
-    this.password = other.password;
-    this.notes = other.notes;
-    this.category = other.category;
-}
+    public Credential(String username, String password, String notes) {
+        this.username = username;
+        this.password = password;
+        this.notes = notes;
+    }
 
-public String getSite(){
-    return site;
-}
+    public Credential(Credential other) {
+        this.username = other.username;
+        this.password = other.password;
+        this.notes = other.notes;
+    }
 
-public String getUsername(){
-    return username;
-}
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getNotes() { return notes; }
 
-public String getPassword() {
-    return password;
-}
+    public void setUsername(String username) { this.username = username; }
+    public void setPassword(String password) { this.password = password; }
+    public void setNotes(String notes) { this.notes = notes; }
 
-public String getNotes() {
-    return notes;
-}
-
-public String getCategory() {
-    return category;
-}
-
-public void setSite(String site){
-    this.site = site;
-}
-
-public void setUsername(String username) {
-    this.username = username;
-}
-
-public void setPassword(String password) {
-    this.password = password;
-}
-
-public void setNotes(String notes) {
-    this.notes = notes;
-}
-
-public void setCategory(String category) {
-    this.category = category;
-}
+    public abstract String getCategory();
 }
