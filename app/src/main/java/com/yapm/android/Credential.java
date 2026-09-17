8753a313b27a80441886c3b10f784ec500bead26
Aditya Vikram <adityavikram803@gmail.com>
2026-09-17 09:18:23 +0530

    Add static count method to Credential class

--- app/src/main/java/com/yapm/android/Credential.java
+++ app/src/main/java/com/yapm/android/Credential.java
@@ -25,6 +25,7 @@ public abstract class Credential {
     public String getUsername() { return username; }
     public String getPassword() { return password; }
     public String getNotes() { return notes; }
+    public static void count(){ System.out.println(count);}
 
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
